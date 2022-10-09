99f0808cc88fb9cbbffc923dd97a14db8d0edb2a
kafri8889 <kafri8889@gmail.com>
2022-10-09 09:43:22 +0700

    New `Constant`

---
+++ app/src/main/java/com/daniellemarsh/ujianbro/data/Constant.kt
@@ -0,0 +1,8 @@
+package com.daniellemarsh.ujianbro.data
+
+object Constant {
+	
+	const val E_LEARNING_URL = "https://kafri8889.github.io/data/ujianbro/exambro_web_url.txt"
+	const val LATEST_APP_VERSION_URL = "https://kafri8889.github.io/data/ujianbro/current_app_version_code.txt"
+	
+}
